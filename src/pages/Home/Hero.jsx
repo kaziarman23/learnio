a35afda52e6b0e0f1d0e6ae8ef43521aacaaddef
Kazi Arman <kaziarman@proton.me>
2025-09-16 16:24:10 +0600

    Update Hero section image

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -19,19 +19,19 @@ const Hero = () => {
           {/* Image content */}
           <div className="flex w-full flex-col items-center justify-center gap-3 lg:w-1/2">
             <img
-              src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.nea.org%2Fsites%2Fdefault%2Ffiles%2Flegacy%2F2020%2F04%2Fnew_teacher.jpeg&f=1&nofb=1&ipt=0d3e1ea6475419185788530ea3dcd8f08f858cae50f60cddb849f525d99f1678&ipo=images"
-              alt=""
+              src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn-wordpress-info.futurelearn.com%2Fwp-content%2Fuploads%2F1_Different-teaching-methods-and-how-to-use-them.jpg.optimal.jpg&f=1&nofb=1&ipt=aaf72edc8449a687dfcf43497651d184629206356801bae86847610daa864717"
+              alt="hero section image 1"
               className="h-1/2 w-5/6 rounded-2xl border-2 border-black object-cover"
             />
             <div className="flex items-center justify-center gap-5">
               <img
                 src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp11867996.jpg&f=1&nofb=1&ipt=88948f2abbd08c06768368415796843c6e37a78140d7abcd1b372619795b611a&ipo=images"
-                alt=""
+                alt="hero section image 2"
                 className="h-full w-1/3 rounded-2xl border-2 border-black object-cover"
               />
               <img
                 src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn8.dissolve.com%2Fp%2FD2115_185_452%2FD2115_185_452_1200.jpg&f=1&nofb=1&ipt=73d53eec312d3e51f3e82ca61990be4c06a468a570a208ba15e0ccd1c580ebc7&ipo=images"
-                alt=""
+                alt="hero section image 3"
                 className="h-full w-1/3 rounded-2xl border-2 border-black object-cover"
               />
             </div>
